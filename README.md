7b8f978f8372ffccd123849b8812e2c1462350d5
Rodhi Faisal Mufid <almufid.business@gmail.com>
2024-04-22 00:29:04 +0700

    About Project Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Welcome to About Me
+
+Have a nice day :)
---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+
+const container = document.querySelector(".info-box");
+const toggle = document.querySelector(".toggle");
+toggle.addEventListener("click", () => {
+  container.classList.toggle("active");
+});
+
+function vibrate() {
+  navigator.vibrate(6);
+}
+
+const socialMediaLinks = document.querySelectorAll(".social-media div");
+
+ScrollReveal().reveal(socialMediaLinks, {
+  duration: 1000,
+  opacity: 0,
+  distance: "30%",
+  origin: "bottom",
+  scale: 0.9
+});
