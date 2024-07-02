b6080e0c0e1447788902bd6d249e6d163c1edbd1
Rodhi Faisal Mufid <almufid.business@gmail.com>
2024-07-02 12:54:04 +0700

    UpdateHeader

--- assets/js/script.js
+++ assets/js/script.js
@@ -61,4 +61,35 @@ document.getElementById('contact-form').addEventListener('submit', function(even
 });
 
 
+// Search
+
+const searchBtn = document.getElementById('search-btn');
+const searchDropdown = document.getElementById('search-dropdown');
+const searchForm = document.getElementById('search-form');
+const content = document.querySelector('.content');
+
+searchBtn.addEventListener('click', function() {
+  searchDropdown.style.display = (searchDropdown.style.display === 'block') ? 'none' : 'block';
+});
+
+document.addEventListener('click', function(event) {
+  if (!searchBtn.contains(event.target) && !searchDropdown.contains(event.target)) {
+     searchDropdown.style.display = 'none';
+   }
+});
+
+searchForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+  let searchTerm = document.getElementById('search-input').value.toLowerCase();
+  let paragraphs = content.querySelectorAll('p');
+	  
+  paragraphs.forEach(p => {
+     p.style.background = '';
+     if (p.textContent.toLowerCase().includes(searchTerm)) {
+       p.style.background = 'yellow';
+      }
+ });
+
+  document.getElementById('search-input').value = '';
+});
 
